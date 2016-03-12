8397f484605640cbcf0d8bf7bbe767a0f9c5cc80
dasha2893 <missis.Bobiakowa@yandex.ru>
2016-03-12 21:32:47 +0300

    edited the scripts to deploy a cluster

--- vms/install-spark.sh
+++ vms/install-spark.sh
@@ -9,3 +9,26 @@ sudo wget http://apache-mirror.rbc.ru/pub/apache/spark/spark-1.6.0/spark-1.6.0-b
 cd /home/vagrant
 sudo tar zxvf spark-1.6.0-bin-hadoop2.6.tgz
 sudo rm spark-1.6.0-bin-hadoop2.6.tgz
+sudo mv spark-1.6.0-bin-hadoop2.6 spark
+
+sudo echo "export SPARK_HOME=/home/vagrant/spark" >> ~/.profile && export SPARK_HOME=/home/vagrant/spark
+
+sudo echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> ~/.profile && export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
+sudo chmod 777 -R /home/vagrant/spark/
+
+cd /home/vagrant/spark
+
+sudo cp conf/spark-env.sh.template conf/spark-env.sh && sudo chmod 777 conf/spark-env.sh 
+echo "SPARK_MASTER_IP='node1'" > conf/spark-env.sh 
+
+sudo cp conf/spark-defaults.conf.template conf/spark-defaults.conf && sudo chmod 777 conf/spark-defaults.conf 
+echo "spark.master		spark://node1:7077" > conf/spark-defaults.conf
+echo "spark.executor.memory	1g" >> conf/spark-defaults.conf
+echo "spark.eventLog.enabled	true" >> conf/spark-defaults.conf
+echo "spark.eventLog.dir	file:///tmp/" >> conf/spark-defaults.conf
+echo "spark.serializer       	org.apache.spark.serializer.KryoSerializer" >> conf/spark-defaults.conf
+
+sudo cp conf/slaves.template conf/slaves && sudo chmod 777 conf/slaves 
+echo "node2" > conf/slaves 
+echo "node3" >> conf/slaves
--- vms/install-sshkey.sh
+++ vms/install-sshkey.sh
@@ -2,7 +2,10 @@
 !/bin/sh
 
 if [ ! -f ~/.ssh/id_rsa_vagrant.pub ]; then
-    cp /home/vagrant/project/vms/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa_vagrant.pub;
+    cp /home/vagrant/project/vms/sshkey/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub;
+    cp /home/vagrant/project/vms/sshkey/id_rsa /home/vagrant/.ssh/id_rsa;
+    sudo chown vagrant /home/vagrant/.ssh/id_rsa
+    sudo chown vagrant /home/vagrant/.ssh/id_rsa.pub
     cat /home/vagrant/project/vms/sshkey/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
     echo 'The key from sshkey has been succesfully installed'
 else
